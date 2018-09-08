c8e63ffb906ede918a6acef6a8ab558da78e57d7
Jeremy G Monger <jeremymong@gmail.com>
2018-09-08 15:36:14 -0400

    feat: Add makeGrid function

--- designs.js
+++ designs.js
@@ -2,9 +2,17 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+function makeGrid(rows, cols) {
+  const pixelCanvas = document.getElementById('pixelCanvas');
 
-function makeGrid() {
-
-// Your code goes here!
+  for (let row = 1; row <= rows; row++){
+    let newRow = document.createElement('tr');
+    for(let col = 1; col <= cols; col++){
+      let newCol = document.createElement('td');
+      newRow.appendChild(newCol);
+    }
+    pixelCanvas.appendChild(newRow);
+  }
 
 }
+makeGrid(6,6);
