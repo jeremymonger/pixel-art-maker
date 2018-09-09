1688c4837641159ec1ce0d87ed369e26be5cbd8d
Jeremy G Monger <jeremymong@gmail.com>
2018-09-09 13:35:50 -0400

    fix: Reset grid on each call

--- designs.js
+++ designs.js
@@ -1,18 +1,19 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
 function makeGrid(rows, cols) {
+// Takes two numbers, and uses them to make a grid/table.
   const pixelCanvas = document.getElementById('pixelCanvas');
+  pixelCanvas.innerHTML = ''; // Clears table, allowing users to 'reset'
 
   for (let row = 1; row <= rows; row++){
     let newRow = document.createElement('tr');
+
     for(let col = 1; col <= cols; col++){
       let newCol = document.createElement('td');
       newRow.appendChild(newCol);
     }
+
     pixelCanvas.appendChild(newRow);
   }
 
 }
 makeGrid(6,6);
+makeGrid(1,4);
